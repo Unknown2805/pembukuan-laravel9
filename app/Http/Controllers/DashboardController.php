200a1d767aae26fcf8206459c3915e38d9253fe9
Unknown2805 <nuraliali2805@gmail.com>
2022-10-27 12:58:29 +0700

    pembukuan finish

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,82 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Models\Masuk;
+use App\Models\Keluar;
 
 class DashboardController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        return view('dashboard.index');
+            $masuk = Masuk::all();
+            $keluar = Keluar::all();
+
+            $this_year = Carbon::now()->format('Y');
+            $month_in = Masuk::where('tanggal','like', $this_year.'%')->get();
+            $month_out = Keluar::where('tanggal','like', $this_year.'%')->get();
+
+            for ($i=1; $i <= 12; $i++){
+                $data_month_in[(int)$i]=0;
+            }
+    
+            foreach ($month_in as $a) {
+                $bulan_in= explode('-',carbon::parse($a->created_at)->format('Y-m-d'))[1];
+                $data_month_in[(int) $bulan_in]+= $a->jumlah; 
+            }
+
+            for ($i=1; $i <= 12; $i++){
+                $data_month_out[(int)$i]=0;
+            }
+    
+            foreach ($month_out as $a) {
+                $bulan_out= explode('-',carbon::parse($a->created_at)->format('Y-m-d'))[1];
+                $data_month_out[(int) $bulan_out]+= $a->jumlah; 
+            }
+
+            $total_in = $masuk->sum('jumlah');
+            $total_out = $keluar->sum('jumlah');
+            $sisa = $total_in - $total_out;
+            return view('dashboard.index',compact('total_in','total_out','sisa'))
+            -> with('data_month_in', $data_month_in)
+            -> with('data_month_out', $data_month_out);
+            ;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,4 +1,155 @@
 @extends('layouts.master')
 @section('main')
- 
+    {{-- preview kas --}}
+        <div class="row">
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-2 col-lg-2  d-flex justify-content-start ">
+                                <div class="stats-icon green">
+                                    <i class="ibi bi-box ms-2 me-2"></i>
+                                </div>
+                            </div>
+                            <div class="col-md-10 col-lg-10 col-xl-12 col-xxl-7">
+                                <h6 class="text-muted font-semibold">Kas Masuk</h6>
+                                
+                                <h6 class="font-extrabold mb-0">Rp. @money((float)$total_in)</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-2 col-lg-2  d-flex justify-content-start ">
+                                <div class="stats-icon red">
+                                    <i class="ibi bi-box ms-2 me-2"></i>
+                                </div>
+                            </div>
+                            <div class="col-md-10 col-lg-10 col-xl-12 col-xxl-7">
+                                <h6 class="text-muted font-semibold">Kas Keluar</h6>
+                                
+                                <h6 class="font-extrabold mb-0">Rp. @money((float)$total_out)</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-2 col-lg-2  d-flex justify-content-start ">
+                                <div class="stats-icon blue">
+                                    <i class="ibi bi-box ms-2 me-2"></i>
+                                </div>
+                            </div>
+                            <div class="col-md-10 col-lg-10 col-xl-12 col-xxl-7">
+                                <h6 class="text-muted font-semibold">Sisa Kas</h6>
+                                
+                                <h6 class="font-extrabold mb-0">Rp. @money((float)$sisa)</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    {{-- chart --}}
+    <div class="col-12 col-md-12">
+        <div class="card shadow">
+            <div class="card-header">
+                <h6>Rekap Penjualan Product</h6>
+            </div>
+            <div class="card-body">
+
+                <div class="row">
+
+
+                    <div class="col-12 col-md-12">
+                        <div class="text-center">
+                            <canvas id="products_b"></canvas>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.14.305/pdf.min.js"
+            integrity="sha512-dw+7hmxlGiOvY3mCnzrPT5yoUwN/MRjVgYV7HGXqsiXnZeqsw1H9n9lsnnPu4kL2nx2bnrjFcuWK+P3lshekwQ=="
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    {{-- script chart --}}
+    <script>
+            const b_products = [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'Aug',
+                'Sep',
+                'Oct',
+                'Nov',
+                'Dec'
+            ];
+
+            const b_productsd = {
+                labels: b_products,
+                datasets: [{
+                    label: 'Netto',
+                    backgroundColor: '#43beaf',
+                    borderRadius: 4,
+                    barThickness: 10,
+
+                    data: [
+                        @foreach ($data_month_in as $ikm)
+                            {{ $ikm }},
+                        @endforeach
+                    ]
+                }, {
+                    label: 'Loss',
+                    backgroundColor: '#dc3545',
+                    borderRadius: 4,
+                    barThickness: 10,
+                    data: [
+                        @foreach ($data_month_out as $okm)
+                            {{ $okm }},
+                        @endforeach
+                    ],
+                }]
+            };
+
+            const bar_products = {
+                type: 'bar',
+                data: b_productsd,
+                options: {
+                    responsive: true,
+                    indexAxis: 'x',
+                    plugins: {
+                        legend: {
+                            position: 'bottom',
+                            labels: {
+                                usePointStyle: true,
+                            },
+                        },
+                    },
+                }
+            };
+    </script>
+    <script>
+            const bulanan_products = new Chart(
+                document.getElementById('products_b'),
+                bar_products
+            );
+    </script>
 @endsection
